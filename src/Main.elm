862bbeedbd6c90cdc482212af9f5e619be4473f6
Eric <ericc3141@users.noreply.github.com>
2020-03-26 00:34:38 -0700

    receive keyboard event

--- src/Main.elm
+++ src/Main.elm
@@ -7,6 +7,8 @@ import Element as El exposing (Element)
 import Element.Background as Elbg
 import Element.Input as Elin
 import Html exposing (Html)
+import Html.Events as HtmlE
+import Json.Decode as JsonD
 
 
 main =
@@ -19,8 +21,9 @@ main =
 
 
 type alias Model =
-    { cells : Dict Int Expr
-    , pos : Int
+    { above : List Expr
+    , below : List Expr
+    , curr : String
     }
 
 
@@ -31,45 +34,45 @@ type Expr
 type Msg
     = Push String
     | In String
+    | Key String
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { cells = Dict.empty, pos = 0 }
+    ( { above = [ Num 1 ], below = [ Num -1 ], curr = "" }
     , Cmd.none
     )
 
 
-viewCell : ( Int, Expr ) -> Element Msg
-viewCell ( idx, expr ) =
+viewCell : Expr -> Element Msg
+viewCell expr =
     case expr of
         Num val ->
-            El.text <| String.fromFloat val
+            El.el
+                [ El.width <| El.px 300
+                , Elbg.color (El.rgb 0 0.1 0.5)
+                ]
+            <|
+                El.text <|
+                    String.fromFloat val
 
 
 view : Model -> Document Msg
-view { cells, pos } =
+view { above, below, curr } =
     let
-        input : Element Msg
-        input =
-            Elin.text []
+        cellsView : Element Msg
+        cellsView =
+            Elin.text
+                [ El.centerX
+                , El.centerY
+                , El.above <| El.column [] <| List.map viewCell above
+                , El.below <| El.column [] <| List.map viewCell below
+                ]
                 { label = Elin.labelHidden "input"
                 , onChange = In
                 , placeholder = Nothing
-                , text = ""
+                , text = curr
                 }
-
-        cellsView : Element Msg
-        cellsView =
-            El.column
-                [ Elbg.color (El.rgb 0 0.5 0.1)
-                , El.width <| El.fillPortion 2
-                , El.height El.fill
-                , El.inFront input
-                ]
-            <|
-                List.map viewCell <|
-                    Dict.toList cells
 
         controlsView : Element Msg
         controlsView =
@@ -90,9 +93,23 @@ view { cells, pos } =
                 , El.text "hello world"
                 ]
 
+        keyDown : El.Attribute Msg
+        keyDown =
+            El.htmlAttribute
+                (HtmlE.on "keydown"
+                    (JsonD.field "key"
+                        (JsonD.string |> JsonD.map Key)
+                    )
+                )
+
         root : Html Msg
         root =
-            El.layout [ El.explain Debug.todo ] <| El.row [] [ cellsView, controlsView ]
+            El.layout [ keyDown ] <|
+                El.row
+                    [ El.height El.fill
+                    , El.width El.fill
+                    ]
+                    [ cellsView, controlsView ]
     in
     { title = "hello world", body = [ root ] }
 
@@ -104,6 +121,9 @@ update msg model =
             ( model, Cmd.none )
 
         In str ->
+            ( { model | curr = str }, Cmd.none )
+
+        Key str ->
             ( model, Cmd.none )
 
 
